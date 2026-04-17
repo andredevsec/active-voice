6a7d26cf101ef909212f29c96263c5efb1082ba6
andredevsec <andre.all3@hotmail.com>
2026-04-17 11:34:07 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'active-voice',
+  webDir: 'www'
+};
+
+export default config;
